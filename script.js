0122ab24fe3384b92f62a3049f96df13ffc2e50c
Saad0095 <saadkhalid0095@gmail.com>
2024-06-23 20:39:40 +0500

    Created Rock-Paper-Scissor Game

---
+++ script.js
@@ -0,0 +1,87 @@
+let userScoreText = document.querySelector("#user-score")
+let compScoreText = document.querySelector("#comp-score")
+let resetBtn = document.querySelector(".resetButton")
+let msg = document.querySelector("#msg")
+
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice")
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id")
+        playGame(userChoice);
+    });
+});
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"]
+    const randIndex = Math.floor(Math.random() * 3);
+    return options[randIndex]
+}
+
+const playGame = (userChoice) => {
+    const compChoice = genCompChoice();
+
+    if (userChoice === compChoice) {
+        msg.innerHTML = "Draw!"
+        msg.style.color = "#414141"
+
+    }
+
+    else {
+        let userWin = true;
+
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+
+            /** Short for this **/
+
+            /*if (compChoice === "paper") {
+                userWin = false
+            }
+            else {
+                userWin = true
+            }*/
+        }
+
+        else if (userChoice === "paper") {
+            userWin = compChoice === "scissor" ? false : true;
+        }
+
+        else if (userChoice === "scissor") {
+            userWin = compChoice === "rock" ? false : true;
+        }
+
+        showWinner(userWin, compChoice)
+    }
+}
+
+const showWinner = (userWin, compChoice) => {
+    if (userWin) {
+        userScore++;
+        userScoreText.innerHTML = userScore;
+        msg.innerHTML = `You Win! Computer choosed ${compChoice}`
+        msg.style.color = "green"
+    }
+    else {
+        compScore++;
+        compScoreText.innerHTML = compScore;
+        msg.innerHTML = `You Loose! Computer choosed ${compChoice}`
+        msg.style.color = "red"
+
+    }
+}
+
+function resetGame() {
+    userScore = 0;
+    compScore = 0;
+    userScoreText.innerHTML = userScore;
+    compScoreText.innerHTML = compScore;
+    msg.innerHTML = "Your Move"
+}
+
+resetBtn.addEventListener("click", () => {
+    resetGame();
+})
